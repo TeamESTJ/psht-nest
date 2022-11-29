6b6342dc3911d191c66ae69132f244ec69410b64
Wonsang <temporaryaion1@hotmail.com>
2022-11-29 22:02:05 +0900

    remove comments and rename file

--- src/report/tests/report.e2e.spec.ts
+++ src/report/tests/report.e2e.spec.ts
@@ -1,4 +1,4 @@
-// import { HttpStatus } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { makeRequest } from '../../../test/utils/make-request';
 import { DataSource } from 'typeorm';
@@ -35,34 +35,9 @@ describe('Report module', () => {
           content: reportContent,
         });
 
-        expect(response.status).toBe(201);
+        expect(response.status).toBe(HttpStatus.CREATED);
         expect(response.body.content).toEqual(reportContent);
       });
-
-      // it('should create dog and save it to db', async () => {
-      //   const given: CreateDogDto = {
-      //     name: 'Dingo',
-      //     age: 3,
-      //     breed: 'Beagle',
-      //   };
-
-      //   const response = await makeRequest(app).post('/dogs').send(given);
-
-      //   expect(response.status).toBe(HttpStatus.CREATED);
-      //   expect(response.body.id).toBeDefined();
-
-      //   // check if saved dog in database has same properties sent via POST request
-      //   const found = await connection
-      //     .getRepository(DogPostgresEntity)
-      //     .findOne(response.body.id);
-
-      //   expect(found).toMatchObject({
-      //     id: response.body.id,
-      //     name: given.name,
-      //     age: given.age,
-      //     breed: given.breed,
-      //   });
-      // });
     });
   });
 });
